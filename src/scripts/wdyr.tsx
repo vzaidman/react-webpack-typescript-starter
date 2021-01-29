547dbf1e5c6b5300cfdaea3eb0b6f8bf8d782869
vzaidman <vzaidman@gmail.com>
2021-01-29 11:45:28 +0200

    fixed wdyr issue

--- src/scripts/wdyr.tsx
+++ src/scripts/wdyr.tsx
@@ -1,4 +1,6 @@
-import * as React from "react";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import React from "react";
 
 if (process.env.NODE_ENV === "development") {
   const whyDidYouRender = require("@welldone-software/why-did-you-render");
